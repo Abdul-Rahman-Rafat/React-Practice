4da0ecaa77d42afc74ddd940e83efa37d2f76921
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2025-10-17 15:34:29 +0300

    First App with React

---
+++ First App with React/website/my-react-app/src/Food.jsx
@@ -0,0 +1,16 @@
+function Food(){
+const foodItem1 = "Pizza";
+const foodItem2 = "Burger";
+
+
+return(
+
+<ul>
+    <li>{foodItem1+"hat"} </li>
+    <li>{foodItem2.toUpperCase()}</li>
+</ul>
+);
+
+}
+
+export default Food;
